a3c1539e552e081cffd6229099481ed10f29a5fb
dcarvalc <diego.carvalho.castano@everis.com>
2020-03-24 13:56:03 -0300

    Initial commit, setting app locally.

---
+++ TesteiOS/TesteiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteiOS
+//
+//  Created by Diego Carvalho Castano on 24/03/20.
+//  Copyright © 2020 dcarvalc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
